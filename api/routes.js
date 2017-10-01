475ad4dab3449047d4374dfd775031585555add1
Jibran Kalia <jkalia@student.42.us.org>
2017-09-30 19:22:38 -0700

    Fixed cors issues

--- api/routes.js
+++ api/routes.js
@@ -5,12 +5,13 @@ const router = express.Router();
 const database = require('../config/database.js');
 const sentry = require('./project.js');
 
-router.put('/api', (req, res) => {
+router.post('/api', (req, res) => {
   const body = req.body;
+  console.log(body);
   const bucket = body.name;
   database.createTable(bucket)
     .then(() => sentry.getData(body))
-    .then(() => console.log('hi'))
+    .then(() => console.log('requestdone'))
     .then(() => res.send('done'))
     .catch((err) => {
       console.log(err);
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8200;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(require('./api/routes.js'));
 
